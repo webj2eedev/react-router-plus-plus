0fa784de5177e75f4df54c33c4a8242190ca60f5
lzx <lzx@dareway.com.cn>
2021-03-12 17:23:48 +0800

    引入lodash定义，调整demo

--- demo/App/App.tsx
+++ demo/App/App.tsx
@@ -1,19 +1,31 @@
-import React, { useState, useRef } from "react";
-import ReactDOM from "react-dom";
+import React, { useState, useRef, Component } from "react";
+import { PPBrowserRouter } from "../../src";
+import type { PPRoutes } from "../../src";
 
 import "./style/index.css";
+
+const routes: PPRoutes = [{
+  path: "/login",
+  component: function(){
+    return <h1>login</h1>
+  }
+},{
+  path: "/logout",
+  component: function(){
+    return <h1>logout</h1>
+  }
+}];
 
 export default function App() {
   const [count, updateCount] = useState(1);
 
   const onAdd = () => {
-    updateCount((count)=>count+1);
-  }
+    updateCount((count) => count + 1);
+  };
 
   return (
     <>
-      <h1>{count}</h1>
-      <button onClick={onAdd}>add</button>
+      <PPBrowserRouter routes={routes}></PPBrowserRouter>
     </>
   );
 }
