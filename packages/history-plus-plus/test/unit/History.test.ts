f3a834e466e19b3cd553c0da990a443926582175
lzx <lzx@dareway.com.cn>
2021-03-31 19:45:08 +0800

    增加一波测试用例；

--- packages/history-plus-plus/test/unit/History.test.ts
+++ packages/history-plus-plus/test/unit/History.test.ts
@@ -1,7 +1,17 @@
-import {History} from "../../src"
+import { History } from '../../src'
 
 describe('History', () => {
     test('normalizeBase', async () => {
-        const history = new History();
+        const mockOnComplete = jest.fn()
+        const mockOnAbort = jest.fn()
+        const mockListener = jest.fn()
+
+        const history = new History()
+        history.listen(mockListener)
+        history.confirmTransition(null, mockOnComplete, mockOnAbort);
+
+        expect(mockOnComplete).toHaveBeenCalledWith(null);
+        expect(mockListener).toHaveBeenCalledWith(null);
+
     })
 })
